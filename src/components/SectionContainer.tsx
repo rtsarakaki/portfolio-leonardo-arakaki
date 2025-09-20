06ef39cb13d1ca32ee332efd600417cf0e37f785
rtsarakaki <rtsarakaki@gmail.com>
2025-09-20 13:58:15 -0300

    fix: corrigir cota da seção experiências para usar padrão padrão
    - Remover ExperiencesCota personalizado
    - Usar ArchitecturalCota padrão como outras seções
    - Garantir consistência visual entre todas as seções
    - Manter animação de revelação de baixo para cima
    - Remover importação desnecessária

--- src/components/SectionContainer.tsx
+++ src/components/SectionContainer.tsx
@@ -3,7 +3,6 @@
 import React from 'react';
 import { motion } from 'framer-motion';
 import ArchitecturalCota from './ArchitecturalCota';
-import ExperiencesCota from './ExperiencesCota';
 
 interface SectionContainerProps {
   sectionId: string;
@@ -133,11 +132,7 @@ const SectionContainer: React.FC<SectionContainerProps> = ({
     if (cotaDirection === 'left' || cotaDirection === 'right') {
       return (
         <div className={`${cotaWidth} flex-shrink-0 relative`}>
-          {isExperiencesSection ? (
-            <ExperiencesCota direction={cotaDirection} />
-          ) : (
-            <ArchitecturalCota direction={cotaDirection} />
-          )}
+          <ArchitecturalCota direction={cotaDirection} />
         </div>
       );
     } else {
